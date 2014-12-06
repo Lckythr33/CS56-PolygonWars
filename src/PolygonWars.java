8d29ab00af379509ca130708e11aed0d548ea295
ncoop <cooper_nathan@student.smc.edu>
2014-12-05 20:05:07 -0800

    make window unresizable

--- src/PolygonWars.java
+++ src/PolygonWars.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PolygonWars extends JFrame 
 {
@@ -145,6 +146,7 @@ public class PolygonWars extends JFrame
         win.setTitle("Polygon Wars");
 		win.setVisible(true);
         win.setLocationRelativeTo(null);
+        win.setResizable(false);
 		win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	} 
 }
